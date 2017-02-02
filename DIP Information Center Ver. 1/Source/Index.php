5f2603137418295b98fb99c581408e8edf5703db
Lukemuffin <Lukemuffin@users.noreply.github.com>
2017-02-02 10:37:59 -0600

    Add files via upload

---
+++ DIP Information Center Ver. 1/Source/Index.php
@@ -0,0 +1,130 @@
+<html>
+
+	<head> 
+	
+		 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	
+		<link rel="stylesheet" type="text/css" href="../ScriptsStylesFonts/StyleSheet.css">
+		
+		<link rel="icon" type="image/png" href="../Pics/Fav.ico">
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+				
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+		
+		<script src="../ScriptsStylesFonts/Jq.js"></script>
+			 
+	</head>
+	
+	<body>
+	
+	<div class="container">
+		
+			  <div class="jumbotron">
+			  
+					<div id="myCarousel" class="carousel slide" data-ride="carousel">
+					
+					  <ol class="carousel-indicators">
+					  
+						<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+						<li data-target="#myCarousel" data-slide-to="1"></li>
+						<li data-target="#myCarousel" data-slide-to="2"></li>
+						<li data-target="#myCarousel" data-slide-to="3"></li>
+						
+					  </ol>
+
+					 
+					  <div class="carousel-inner" role="listbox">
+						<div class="item active">
+						  <img src="../Pics/.jpg" >  
+						</div>
+
+						<div class="item">
+						 <img src="../Pics/.jpg" >  
+						</div>
+
+						<div class="item">
+						 <img src="../Pics/.jpg" >  
+						</div>
+
+						<div class="item">
+						  <img src="../Pics/.jpg" >  
+						</div>
+					  </div>
+
+				
+					  <a class="left carousel-control"
+							href="#myCarousel" role="button" 
+							data-slide="prev">
+							
+						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						
+						<span class="sr-only">Previous</span>
+						
+					  </a>
+					  
+					  <a class="right carousel-control" 
+							href="#myCarousel" 
+							role="button" 
+							data-slide="next">
+							
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						
+						<span class="sr-only">Next</span>
+						
+					  </a>
+					  
+					</div>
+	         
+			 </div>
+			 		  
+		</div>
+		
+
+	<div class="container">	
+		
+			<nav class="navbar navbar-inverse">
+			
+				  <div class="container-fluid">
+				  
+						<div class="navbar-header">
+						
+							<a class="navbar-brand" > </a>
+						
+						</div>
+						
+							<ul class="nav navbar-nav">
+							
+								  <li class="active"><a href="Home.php">Home</a></li>
+								   							  					
+							</ul>
+							
+							<ul class="nav navbar-nav navbar-right">
+							
+							
+							</ul>
+							
+				  </div>
+				  
+			</nav>
+			
+		</div>
+		
+		
+	<div class="container">
+	
+		<div class="panel panel-heading">
+	
+		<h2 class="text-center"> Information Center </h2>
+		
+		</div>
+		
+		<div class="panel panel-default">
+
+		</div>
+		
+	</div>
+
+	</body>
+
+</html>
